49cdf211e84cb8b766a5536ad26e7d24f9888663
MrBekzod0899 <bekzodqurbonov1399@gmail.com>
2022-03-12 17:45:00 +0500

    send message

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,34 @@ export default function Main() {
   return (
     <div className='main container-fluid'>
       <div className="row w-100">
-        <div className="col-lg-7 col-md-8 col-sm-12 col-12 col-xs-12">
+        
+      {/* <div className="leftContent">
+          <h1>
+            Разаработка сайтов
+            и мобильных приложений
+          </h1>
+          <p>
+            Помогаем бизнесу увеличить прибыль
+            с помощью digital-инструментов
+          </p>
+          <div className="projectOrder">
+            <button className='btn btn-light'>Обсудить проект</button>
+          </div>
+        </div>
+        
+        <div className="rightContent">
+            <div className="bgImage">
+                  <div className="img1">
+
+                  </div>
+                  <div className="img2">
+                    
+                  </div>
+            </div>
+       </div> */}
+      
+
+      <div className="col-xl-7 col-lg-7 col-md-7 col-sm-7 col-12 ">
           <div className="title">
             <h1>
               Разаработка сайтов
@@ -20,17 +47,17 @@ export default function Main() {
             <button className='btn btn-light'>Обсудить проект</button>  
           </div>
         </div>
-        <div className="col-xl-5 col-lg-5 col-md-4 col-12 col-sm-12">
+        <div className="col-xl-5 col-lg-5 col-md-5 col-12 col-sm-5 col-12">
             <div className="bgImage">
                 <div className="Image1">
-                  </div>  
-            </div>
-            <div className="bgImage">
+                  </div>
+              </div>
+            <div className='bgImage'>
               <div className="Image2">
-                </div>  
+              </div>  
             </div>
         </div>  
-      </div>
     </div>
+    </div >
   )
 }
--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -9,54 +9,53 @@ export default function Order() {
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
     const [interest, setInterest] = useState('');
-    const ServiceRef=useRef()
+    const ServiceRef = useRef()
 
     const handleRecieve = () => {
         setSend(true)
         setRecieve(false)
     }
-    const PhoneRefElem=(e)=>{
-    console.log(e.target.value)
-     setPhone(e.target.value)
-       if (e.target.value.length===13){
-          ServiceRef.current.focus()
+    const PhoneRefElem = (e) => {
+        console.log(e.target.value)
+        setPhone(e.target.value)
+        if (e.target.value.length === 13) {
+            ServiceRef.current.focus()
         }
     }
-
     const handleSend = () => {
-        if(name==='' && phone==='' &&  interest===''){
+        if (name === '' && phone === '' && interest === '') {
             document.getElementById('name').classList.add('is-invalid');
             document.getElementById('phone').classList.add('is-invalid');
             document.getElementById('service').classList.add('is-invalid');
         }
-        else{
-             if(name!==''){
+        else {
+            if (name !== '') {
                 document.getElementById('name').classList.remove('is-invalid');
-             }
-             if(phone!=='' && phone.length===13){
+            }
+            if (phone !== '' && phone.length === 13) {
                 document.getElementById('phone').classList.remove('is-invalid');
-             }
-             if(interest!==''){
+            }
+            if (interest !== '') {
                 document.getElementById('service').classList.remove('is-invalid');
-             }
+            }
         }
 
-        if(name && phone && interest){
+        if (name && phone && interest) {
             setSend(false)
             setRecieve(false)
             toast.success("🌟🤩🌟🥳🌟 Oтправлено !!! ");
             setPhone('');
             setInterest('');
             setName('');
-        } 
+        }
     }
     return (
         <div id='Заказать' className='Order container-fluid'>
-            <ToastContainer 
-              autoClose={2000}
+            <ToastContainer
+                autoClose={2000}
             />
             <div className="row">
-                <div className="col-xl-8 col-md-8 col-lg-8 col-sm-8 col-12">
+                <div className="col-xl-8 col-md-8 col-lg-8 col-sm-7 col-12">
                     <div className='orderTitle'>
                         <h3>Сколько стоят услуги</h3>
                     </div>
@@ -73,10 +72,10 @@ export default function Order() {
                         </p>
                     </div>
                 </div>
-                <div className="col-xl-4 col-md-4 col-lg-4 col-sm-4 col-12">
+                <div className="col-xl-4 col-md-4 col-lg-4 col-sm-5 col-12">
                     {
                         recieve &&
-                        <div className="card" style={{transition:'all linear 0.5s'}}>
+                        <div className="card" style={{ transition: 'all linear 0.5s' }}>
                             <div className="card-title">
                                 <h4>Получить
                                     бесплатную
@@ -94,52 +93,63 @@ export default function Order() {
                     }
                     {
                         send &&
-                        <div  className="card send" style={{transition:'all linear 0.5s'}}>
+                        <div className="card send" style={{ transition: 'all linear 0.5s' }}>
+                            <div className=" d-none card-minimum">
+                                <div className="card-title">
+                                    <h4>Получить
+                                        бесплатную
+                                        консультацию
+                                    </h4>
+                                </div>
+                                <div className="card-content">
+                                    В рамках консультации уточним необходимую информацию
+                                    для анализа вашего проекта
+                                </div>
+                            </div>
                             <div className="card-action">
-                                <input 
+                                <input
                                     id='name'
-                                    type='text' 
-                                    className='form-control' 
-                                    onChange={(e)=>setName(e.target.value)} 
-                                    placeholder='Ваше имя' 
+                                    type='text'
+                                    className='form-control'
+                                    onChange={(e) => setName(e.target.value)}
+                                    placeholder='Ваше имя'
                                 />
                                 <div className="invalid-feedback p-0 m-0">
                                     Please enter your name.
                                 </div>
                             </div>
                             <div className="card-action">
-                                <input 
+                                <input
                                     id='phone'
-                                    type='tel' 
-                                    onChange={(e)=>PhoneRefElem(e)}   
-                                    className='form-control' 
-                                    placeholder='номер телефона' 
+                                    type='tel'
+                                    onChange={(e) => PhoneRefElem(e)}
+                                    className='form-control'
+                                    placeholder='номер телефона'
                                 />
                                 <div className="invalid-feedback p-0 m-0">
-                                    Please enter 13 numbers which begin with +
+                                    Please enter 13 numbers
                                 </div>
                             </div>
                             <div className="card-action ">
-                                <input 
-                                  id='service'
-                                  type='text' 
-                                  ref={ServiceRef} 
-                                  onChange={(e)=>setInterest(e.target.value)} 
-                                  className='form-control' aria-required='true' 
-                                  placeholder='какая из услуг вас заинтересовала ?' 
+                                <input
+                                    id='service'
+                                    type='text'
+                                    ref={ServiceRef}
+                                    onChange={(e) => setInterest(e.target.value)}
+                                    className='form-control' aria-required='true'
+                                    placeholder='какая из услуг вас заинтересовала ?'
                                 />
                                 <div className="invalid-feedback p-0 m-0">
                                     Please enter your interests.
                                 </div>
                             </div>
-                            
                             <div className="card-action">
                                 <button type='submit' className='btn w-100 btn-light' onClick={handleSend}>Отправить</button>
                             </div>
                         </div>
                     }
                     {!recieve && !send &&
-                        <div className="card iconCard" style={{transition:'all linear 0.5s'}}>
+                        <div className="card iconCard" style={{ transition: 'all linear 0.5s' }}>
                             <div className="cardHeader text-center">
                                 <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <circle cx="37.5" cy="37.5" r="37.5" fill="#2ECC71" />
